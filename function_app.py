adf1c23d48288d77ae01cb6d3b26fdc920616504
MightyJabe <xxditstylexx@gmail.com>
2024-02-03 06:00:38 +0200

    55

--- function_app.py
+++ function_app.py
@@ -2,29 +2,4 @@ import logging
 import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    name = req.params.get('name', 'there')
-    return func.HttpResponse(f"Hello {name}!", status_code=200)
-
-
-app = func.HttpResponse
-
-@app.route(route="hello", auth_level=func.AuthLevel.FUNCTION)
-def hello(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return func.HttpResponse("Hello, Function!", status_code=200)
