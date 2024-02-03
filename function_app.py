42d1900d53fdc587a9fda39334cdf6bcafae3739
MightyJabe <xxditstylexx@gmail.com>
2024-02-03 06:04:55 +0200

    66

--- function_app.py
+++ function_app.py
@@ -1,5 +1,4 @@
-import logging
 import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("Hello, Function!", status_code=200)
+    return func.HttpResponse(f"Hello, this is a test function.")
