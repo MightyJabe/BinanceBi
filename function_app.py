20dfca916cdb36d5068bebbd7587a1b9ac43f703
MightyJabe <xxditstylexx@gmail.com>
2024-02-08 23:53:03 +0200

    added route history

--- function_app.py
+++ function_app.py
@@ -9,37 +9,43 @@ from urllib.parse import urlencode
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.route(route="main")
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    apiKey = os.environ['BINANCE_API_KEY']
-    secretKey = os.environ['BINANCE_API_SECRET'].encode()
+# Centralize the API key and secret retrieval
+apiKey = os.environ['BINANCE_API_KEY']
+secretKey = os.environ['BINANCE_API_SECRET'].encode()
 
-    # Example: Fetching account information
-    url = 'https://fapi.binance.com/fapi/v2/account'
-
-    # Generate a timestamp in milliseconds
+def generate_params():
     timestamp = int(time.time() * 1000)
-
-    # Create a dictionary of parameters, including the timestamp
-    # You may include other parameters as required by your request
     params = {'timestamp': timestamp}
-
-    # Encode parameters and create a signature
     query_string = urlencode(params)
     signature = hmac.new(secretKey, query_string.encode('utf-8'), hashlib.sha256).hexdigest()
-
-    # Append signature to the parameters
     params['signature'] = signature
+    return params
 
-    # Set up the headers with your API key
-    headers = {
-        'X-MBX-APIKEY': apiKey,
-    }
-
+# Function to send a request to the Binance API and handle the response
+def send_request(url, apiKey, params):
+    headers = {'X-MBX-APIKEY': apiKey}
     try:
-        # Send a GET request with the headers and parameters
         response = requests.get(url, headers=headers, params=params)
         return func.HttpResponse(body=response.text, status_code=response.status_code)
     except Exception as error:
         logging.error(f"Error fetching data from Binance API: {error}")
         return func.HttpResponse("Error fetching data from Binance API", status_code=500)
+    
+@app.route(route="account_info")
+def account_info(req: func.HttpRequest) -> func.HttpResponse:
+    # Endpoint for fetching account information
+    url = 'https://fapi.binance.com/fapi/v2/account'
+    params = generate_params()
+    return send_request(url, params)
+
+@app.route(route="trade_history")
+def trade_history(req: func.HttpRequest) -> func.HttpResponse:
+    # Retrieve symbol and limit from the request query parameters
+    symbol = req.params.get('symbol')
+    limit = req.params.get('limit', '1000')  # Example default limit
+    # Endpoint for fetching user's trade history
+    url = 'https://fapi.binance.com/fapi/v1/userTrades'
+    params = generate_params()
+    # Add specific parameters for the trade history request
+    params.update({'symbol': symbol, 'limit': limit}) if symbol else params
+    return send_request(url, params)
